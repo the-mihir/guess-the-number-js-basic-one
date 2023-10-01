cbc1f347e5c28b50cdffe87ffc2b44bd6de36c61
Mihir Das <mihirkantho@gmail.com>
2023-10-01 18:01:14 +0600

    DRY the code

--- app.js
+++ app.js
@@ -3,43 +3,53 @@
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 
+const displayMessage = function (message) {
+    document.querySelector('.message').textContent = message;
+}
+
 
 document.querySelector('.check').addEventListener('click', function () {
     const guess = Number(document.querySelector('.guess').value);
     console.log(guess)
 
     if (!guess) {
-        document.querySelector('.message').textContent = ' 🚫 Put a valid number!';
+        displayMessage(' 🚫 Put a valid number!');
     }
     else if (guess === secretNumber) {
+        displayMessage(' 🎉 Correct Number!');
         document.querySelector('.number').textContent = secretNumber;
-        document.querySelector('.message').textContent = ' 🎉 You get Correct number';
         document.querySelector('.highscore').textContent = score;
         document.querySelector('body').style.backgroundColor = '#60b347';
-    }
 
+    }
     else if (guess !== secretNumber) {
-        if (score >= 1) {
-            document.querySelector('.message').textContent = guess > secretNumber ? ' 📈 Too High!' : ' 📉 Too Law!';
+        if (score > 1) {
+            displayMessage(guess > secretNumber ? ' 📈 Too High!' : ' 📉 Too Law!')
             score--;
             document.querySelector('.score').textContent = score;
         }
         else {
-            document.querySelector('.message').textContent = ' 🎮 You lost the game!';
-            document.getElementById('check__btn').disabled = true;
+            displayMessage(' 🎮 You lost the game!');
+            document.querySelector('.score').textContent = 0;
         }
+
     }
-});
+}
+
+
+
+);
 
 document.querySelector('.again').addEventListener('click', function () {
     score = 20;
     document.querySelector('.score').textContent = score;
     document.querySelector('.number').textContent = '?';
-    document.querySelector('.message').textContent = 'Start guessing...';
+    displayMessage('Start guessing...');
     document.querySelector('.guess').value = '';
     document.getElementById('check__btn').disabled = false;
     document.querySelector('body').style.backgroundColor = '#222';
     secretNumber = Math.trunc(Math.random() * 20) + 1;
+    document.querySelector('body').style.backgroundColor = '#051f30';
 }
 );
 
